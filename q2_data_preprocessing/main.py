fc169bb8463d11959305414f963e76bac5e8d3e2
S0b3Rr <taosong107@gmail.com>
2024-09-25 19:11:06 +0800

    update

--- q2_data_preprocessing/main.py
+++ q2_data_preprocessing/main.py
@@ -54,4 +54,84 @@ print("accepted")
 
 
 # 5. Write a pattern to match a URL.
-pattern5 = r''
+pattern5 = r'^\w*:\/\/\w+(-|.\w+)*'
+for case in test_cases.CASES5:
+    try:
+        assert (re.match(pattern5, case[0]) is not None) is case[1]
+    except AssertionError as e:
+        print("Error:", case[0])
+        print("Expected:", case[1])
+        print("Actual:", (re.match(pattern5, case[0]) is not None))
+        raise e
+print("accepted")
+
+
+# 6. Write a pattern to validate a US phone number format (e.g., (123) 456-7890)
+pattern6 = r'\(\d{3}\)\s\d{3}-\d{4}'
+for case in test_cases.CASES6:
+    try:
+        assert (re.match(pattern6, case[0]) is not None) is case[1]
+    except AssertionError as e:
+        print("Error:", case[0])
+        print("Expected:", case[1])
+        print("Actual:", (re.match(pattern6, case[0]) is not None))
+        raise e
+print("accepted")
+
+
+# 7. Write a pattern to find a string that starts and ends with the same character.
+pattern7 = r'^(.).*\1'
+for case in test_cases.CASES7:
+    try:
+        assert (re.match(pattern7, case[0]) is not None) is case[1]
+    except AssertionError as e:
+        print("Error:", case[0])
+        print("Expected:", case[1])
+        print("Actual:", (re.match(pattern7, case[0]) is not None))
+        raise e
+print("accepted")
+
+
+# 8. Write a pattern to validate a complex password. 
+#    The password must contain at least one uppercase letter, one lowercase letter, 
+#    one digit, one special character, and be at least 8 characters long.
+pattern8 = r'(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^a-zA-Z\d]).{8,}'
+for case in test_cases.CASES8:
+    try:
+        assert (re.match(pattern8, case[0]) is not None) is case[1]
+    except AssertionError as e:
+        print("Error:", case[0])
+        print("Expected:", case[1])
+        print("Actual:", (re.match(pattern8, case[0]) is not None))
+        raise e
+print("accepted")
+
+
+# 9. Write a regex pattern to identify and 
+#    extract all instances of dates in the format dd-mm-yyyy or yyyy/mm/dd from a given text. 
+#    The pattern should handle both formats in a single regex.
+pattern9 = r'(?:\d{2}-\d{2}-\d{4}|\d{4}\/\d{2}\/\d{2})'
+for case in test_cases.CASES9:
+    try:
+        assert re.findall(pattern9, case[0])[0] == case[1]
+    except AssertionError as e:
+        print("Error:", case[0])
+        print("Expected:", case[1])
+        print("Actual:", re.findall(pattern9, case[0]))
+        raise e
+print("accepted")
+
+
+# 10. Create a regex pattern that matches a valid IPv4 address. 
+#     The address must consist of four octets separated by dots,
+#     where each octet is a number between 0 and 255.
+pattern10 = r''
+for case in test_cases.CASES10:
+    try:
+        assert (re.match(pattern10, case[0]) is not None) is case[1]
+    except AssertionError as e:
+        print("Error:", case[0])
+        print("Expected:", case[1])
+        print("Actual:", (re.match(pattern10, case[0]) is not None))
+        raise e
+print("accepted")
--- q2_data_preprocessing/test_cases.py
+++ q2_data_preprocessing/test_cases.py
@@ -3,16 +3,8 @@ CASES2 = [("apple", True), ("banana", False), ("orange", True), ("grape", False)
 CASES3 = [("test@example.com", True), ("invalid-email", False)]
 CASES4 = [("The price is 100 dollars and 50 cents.", ['100', '50'])]
 CASES5 = [("https://www.example.com", True), ("ftp://example.com", True), ("ftp:/invalid.com", False)]
-
-CASES = [
-    ["Hello", "world", "123abc"],
-    ["apple", "banana", "orange", "grape"],
-    ["test@example.com", "invalid-email"],
-    ["The price is 100 dollars and 50 cents."],
-    ["https://www.example.com", "ftp://example.com"],
-    ["(123) 456-7890", "123-456-7890"],
-    ["radar", "hello", "level"],
-    ["Password1!", "PASSWORD,1!", "Pass1!"],
-    ["I12-05-2023", "2023/06/15", "01-01-2024"],
-    ["192.168.0.1", "256.100.50.25", "172.16.254.1"]
-]
+CASES6 = [("(123) 456-7890", True), ("123-456-7890", False)]
+CASES7 = [("radar", True), ("hello", False), ("level", True)]
+CASES8 = [("Password1!", True), ("PASSWORD", False), ("1!", False), ("Pass1!", False)]
+CASES9 = [("I12-05-2023", "12-05-2023"), ("2023/06/15", "2023/06/15"), ("01-01-2024", "01-01-2024")]
+CASES10 = [("192.168.0.1", True), ("256.100.50.25", False), ("172.16.254.1", True)]
